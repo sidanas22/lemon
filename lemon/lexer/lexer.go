6a983481a203e952876029302385fb9b03f8a1f8
sidanas22 <50417848+sidanas22@users.noreply.github.com>
2023-06-29 09:47:39 +0500

    test: complete initial test code for lexer

--- lemon/lexer/lexer.go
+++ lemon/lexer/lexer.go
@@ -1,6 +1,7 @@
 package lexer
 
-import "go/token"
+// import "go/token"
+import "lemon/lemon/token"
 
 type Lexer struct {
 	input        string
@@ -9,26 +10,56 @@ type Lexer struct {
 	ch           byte // current char under examination
 }
 
-func (l *Lexer) NextToken() token.Token{
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
 
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	l.readChar()
+	return tok
+
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
-    l.readChar()
+	l.readChar()
 	return l
 }
 
 // TODO: add support for utf,unicode
 
-func (l *Lexer) readChar(){
-    if l.readPosition >= len(l.input){
-        // 0 is ASCII code for "NULL"
-        l.ch = 0
-    } else{
-        l.ch = l.input[l.readPosition] 
-    } 
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		// 0 is ASCII code for "NULL"
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
 
-    l.position = l.readPosition
-    l.readPosition += 1
+	l.position = l.readPosition
+	l.readPosition += 1
 }
