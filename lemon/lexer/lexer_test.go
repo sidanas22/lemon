5d7fd248d6dead653e90119710e3f0470a5fc3b8
sidanas22 <50417848+sidanas22@users.noreply.github.com>
2023-07-06 22:51:53 +0500

    fix: correct `lemon` language's syntax mistake

--- lemon/lexer/lexer_test.go
+++ lemon/lexer/lexer_test.go
@@ -9,9 +9,9 @@ func TestNextToken(t *testing.T) {
 	input := `let five = 5;
     let ten = 10;
 
-    let add = fn(x+y){
+    let add = fn(x,y){
         x+y;
-    }
+    };
 
     let result = add(five,ten);
     `
