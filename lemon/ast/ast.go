7e64d23efd5bfe83a12372a7941edf9d5c479929
sidanas22 <50417848+sidanas22@users.noreply.github.com>
2023-12-06 00:29:37 +0500

    feat: parse return statement

--- lemon/ast/ast.go
+++ lemon/ast/ast.go
@@ -60,3 +60,11 @@ func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 //              +-------+-------+
 //              |               |
 //      *ast.Identifier *ast.Expression
+
+type ReturnStatement struct {
+	Token       token.Token // the 'return' token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
